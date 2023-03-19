4492d8e7d1d0eb10074da8ebe4befd46f65b124c
Lnvictor <victorhpereira26@gmail.com>
2023-03-19 17:22:59 -0300

    opracoes matematicas

---
+++ variaveis/main.php
@@ -0,0 +1,10 @@
+<?php
+$idade = 23;
+$dobroDaIdade = $idade * 2;
+$idadeAoQuadrado = $idade ** 2;
+$resto = $idade % 2;
+$divisaoInteira = intdiv($idade, 2);
+
+echo $idade;
+echo "\n";
+echo $divisaoInteira;
