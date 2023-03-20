2252aa8a615497f2b1d7f30447648d91aeddb0ca
Lnvictor <victorhpereira26@gmail.com>
2023-03-20 08:56:58 -0300

    tipos de dados

---
+++ variaveis/data_types.php
@@ -0,0 +1,15 @@
+<?php
+
+$idade = 21;
+$salario = 1300.78;
+$souRico = false;
+$nomeCompleto = "Victor Henrique Barbosa Pereira";
+
+// Tipos primitivos
+echo gettype($idade);
+echo "\n";
+echo gettype($salario);
+echo "\n";
+echo gettype($souRico);
+echo "\n";
+echo gettype($nomeCompleto);
